e6a4013c020ad672dd7eb8629378c0250a2e5814
shimamura809 <shimamura@mobcom.ait.kyushu-u.ac.jp>
2017-01-10 17:46:55 +0900

    スライドショー機能の追加・メール周り変更

--- mail.py
+++ mail.py
@@ -1,8 +1,11 @@
 import subprocess
 
-def mail_to_user():
-    message = "echo お水をください・・・、ページへ飛んでね・・・"
-    url     = "ウェブページだよ"
-    subject = "[至急]助けて・・・"
+def mail_to_user(threshold,moisture):
+    message = "echo 'お水をください・・・\nWEBページへお願いします\n'"
+    url     = "'⇒[http://210.152.14.37/AIoT/datalist/]'"
+    th      = "'\n閾値　　　　：'" + str(threshold)
+    mo      = "'\n現在の水分量：'" + str(moisture)
+    subject = "[至急]水分が不足しています・・・"
     to_add  = " shimamura@mobcom.ait.kyushu-u.ac.jp"
-    subprocess.getoutput(message+" "+url+" | mail -s "+ subject + " " +to_add)
+    print("mail")
+    subprocess.getoutput(message+" "+url+th+mo+"  | mail -s "+ subject + " " +to_add)
